e907c5c202fb414aaf8f80c5f78a3ab442dd0660
iamHareen <helanm06@gmail.com>
2024-12-27 16:19:51 +0530

    config the cucumber

---
+++ src/test/features/login.feature
@@ -0,0 +1,7 @@
+Feature: Login Page Validation
+
+    Scenario: login with valid username and password
+    Given Provide valid url
+    When Provide valid username and password
+    Then Click on login button
+
---
+++ src/test/steps/login.spec.ts
@@ -0,0 +1,14 @@
+import {Given, When, Then} from '@cucumber/cucumber';
+       
+         Given('Provide valid url', function () {
+           console.log('Given')
+         });
+       
+         When('Provide valid username and password', function () {
+           console.log('When')
+         });
+       
+         Then('Click on login button', function () {
+           console.log('Then')
+         });
+       
