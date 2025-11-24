3507d6e599033e325f15b3c13fbba654ba01098d
iamHareen <helanm06@gmail.com>
2025-11-24 19:59:14 +0530

    add login, register, profile, and edit profile pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,10 @@ import FooterComponent from './component/common/Footer';
 import AllRoomsPage from './component/booking_rooms/AllRoomsPage';
 import FindBookingPage from './component/booking_rooms/FindBookingPage';
 import RoomDetailsPage from './component/booking_rooms/RoomDetailsPage';
-
+import LoginPage from './component/auth/LoginPage';
+import RegisterPage from './component/auth/RegisterPage';
+import ProfilePage from './component/profile/ProfilePage';
+import EditProfilePage from './component/profile/EditProfilePage';
 
 function App() {
   return (
@@ -22,6 +25,13 @@ function App() {
             <Route path="/rooms" element={<AllRoomsPage />} />
              <Route path="/find-booking" element={<FindBookingPage />} />
              <Route path="/room-details-book/:roomId" element={<RoomDetailsPage />} />
+
+             <Route path="/login" element={<LoginPage />} />
+             <Route path="/register" element={<RegisterPage />} />
+
+             <Route path="/profile" element={<ProfilePage />} />
+             <Route path="/edit-profile" element={<EditProfilePage />} />
+
             <Route path="*" element={<h1>404 Not Found</h1>} />
           </Routes>
         </div>   
