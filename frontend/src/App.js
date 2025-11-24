6cf2923f49c3e700efc1a56147c90336d5f46711
iamHareen <helanm06@gmail.com>
2025-11-24 16:29:15 +0530

    add pagination, rooms page, booking search, and room details pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './component/common/Navbar'; 
 import HomePage from './component/home/HomePage';
 import FooterComponent from './component/common/Footer';
+import AllRoomsPage from './component/booking_rooms/AllRoomsPage';
+import FindBookingPage from './component/booking_rooms/FindBookingPage';
+import RoomDetailsPage from './component/booking_rooms/RoomDetailsPage';
 
 
 function App() {
@@ -16,6 +19,10 @@ function App() {
           <Routes>
             <Route path="/" element={<h1> Hello </h1>} />
             <Route path="/home" element={<HomePage />} />
+            <Route path="/rooms" element={<AllRoomsPage />} />
+             <Route path="/find-booking" element={<FindBookingPage />} />
+             <Route path="/room-details-book/:roomId" element={<RoomDetailsPage />} />
+            <Route path="*" element={<h1>404 Not Found</h1>} />
           </Routes>
         </div>   
         <FooterComponent />
